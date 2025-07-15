80d99a32859f46c27bc66e5b66be68ff33a73cc3
Noah Gordon <noah.o.gordon@gmail.com>
2025-07-15 15:29:57 +0200

    removed header font-weight

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -18,7 +18,7 @@ export default async function MoviePage({ params }: { params: tParams }) {
     return (
       <section className='flex flex-col flex-grow'>
           <div className='p-4 md:pt-8 flex flex-col max-w-7xl mx-auto'>
-              <h2 className='text-3xl md:text-4xl lg:text-5xl mb-1 md:mb-2 lg:mb-3 font-medium'>
+              <h2 className='text-3xl md:text-4xl lg:text-5xl mb-1 md:mb-2 lg:mb-3'>
                 {movie.title || movie.name}
               </h2>
               <div className='flex flex-row text-xs md:text-sm text-gray-400 mb-3'>
