6d0a207e3e2f086ee827d64c8f1d43d381ab6adc
Noah Gordon <noah.o.gordon@gmail.com>
2025-07-21 10:42:03 +0200

    removed comment-out code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-//import { TrendingMovies } from '@/components/movies/TrendingMovies';
-
-//import Hero from '@/components/hero/Hero';
 import HeroSlider from '@/components/HeroSlider';
 import { TrendingList } from '@/components/TrendingList';
 import { fetchTrendingShows, fetchTrendingMovies } from '@/utils/api';
@@ -9,8 +6,6 @@ export default function Home() {
   return (
     <main className='flex-grow'>
       <HeroSlider />
-      {/* <Hero /> */}
-      {/* <TrendingMovies title={'Trending Movies'} /> */}
       <section className='flex flex-col md:flex-row'>
         <TrendingList listTitle={'Trending Movies'} fetchData={fetchTrendingMovies} />
         <TrendingList listTitle={'Trending Shows'} fetchData={fetchTrendingShows} />
